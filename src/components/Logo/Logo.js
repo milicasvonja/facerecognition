231068352176d71cc24edf24b832d1f3be38d893
milicasvonja <37382220+milicasvonja@users.noreply.github.com>
2024-01-24 18:17:31 +0100

    Added logo and signOut for navigation bar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import "./Logo.css";
+import brain from "./brain.png";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 150 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3 tc">
+          <img
+            style={{ paddingTop: "5px", height: 100, width: 100 }}
+            alt="logo"
+            src={brain}
+          ></img>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
