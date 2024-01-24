50cea923724a01f3905397bc6c65f820710dc81a
milicasvonja <37382220+milicasvonja@users.noreply.github.com>
2024-01-25 00:45:48 +0100

    Added logo and Form for link

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="purple f5">{"Milica your current rank is..."}</div>
+      <div className="purple f3">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
