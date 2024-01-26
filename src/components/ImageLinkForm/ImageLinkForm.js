ab8ec7973f28a56b3f1645b38b4108b42ff0b07c
milicasvonja <37382220+milicasvonja@users.noreply.github.com>
2024-01-26 01:37:25 +0100

    Using clarifai API I got dimension of face on photo

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f5">
@@ -9,8 +9,15 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 h-10" type="text"></input>
-          <button className="button w-30 h-10 grow f5 link ph3 pv2 dib black">
+          <input
+            className="f4 pa2 w-70 h-10"
+            type="text"
+            onChange={onInputChange}
+          ></input>
+          <button
+            className="button w-30 h-10 grow f5 link ph3 pv2 dib black"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
